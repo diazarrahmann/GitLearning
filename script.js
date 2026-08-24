02d30e70500557d649025d950e965291c809b0e5
diaz thaufiqurahman <diazarrahman911@gmail.com>
2026-08-24 15:39:48 +0700

    menambahkan fitur kolom inputan, button kirim, reset, fitur ubah tema mode backgorund, notif dengan sapaan, serta function di file java script terbaru

---
+++ script.js
@@ -0,0 +1,47 @@
+function tegur(){
+    let  inputElemen = document.getElementById("YouName");
+    let  nama = inputElemen.value.trim();
+
+    if (nama === ""){
+        alert("Mohon diisi terlebih dahulu!");
+        return;
+    }
+
+    let waktu = new Date().getHours();
+    let ucapan;
+
+    if (waktu < 12) {
+        ucapan = "Good Morning";
+    } else if ( waktu < 15){
+        ucapan = "Good Afternoon";
+    } else if ( waktu < 18){
+        ucapan = "Good Evening";
+    } else {
+            "Good Night"
+    }
+
+    let pesan = ucapan + ", " + nama +"! Selamat Datang.";
+
+    alert(pesan);
+    document.getElementById("hasil").innerText = nama ;
+    inputElemen.value ="";
+}
+
+function darkMode(){
+    document.body.classList.toggle("dark");
+
+    let button = document.getElementById("themeButton");
+
+    if (document.body.classList.contains("dark")) {
+        button.innerText = "☀️ Light Mode";
+    } else {
+        button.innerText = "🌙 Dark Mode"
+    }
+    /*let button = document.querySelector("button[onclick='darkMode()']");
+    button.textContent = document.body.classList.contains("dark") ? "☀️ Light Mode" : "🌙 Dark Mode";*/
+}
+
+function resetForm(){
+    document.getElementById("YouName").value = "";
+    document.getElementById("hasil").innerText = "";
+}
